fed41ffec681bb1c8172d2946e151b0d11363df2
Vitor Vieira <vitor.vieira>
2021-02-25 17:32:00 -0300

    feat: config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
-  const port = 3000;
+
+  const port = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
-  logger.log(`app listening on port ${3000} `);
+  logger.log(`app listening on port ${port} `);
 }
 bootstrap();
